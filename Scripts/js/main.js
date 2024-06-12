26fcbd974a381c4ce9e3a63c27be2062721d1083
Ricardo Vargas <122387515+RicardoVMon@users.noreply.github.com>
2024-06-12 19:54:27 +0000

    Application running correctly

--- Scripts/js/main.js
+++ Scripts/js/main.js
@@ -1,11 +1,12 @@
 const firebaseConfig = {
-    apiKey: APIKEY,
-    authDomain: AUTHDOMAIN,
-    projectId: PROJECTID,
-    storageBucket: STORAGEBUCKET,
-    messagingSenderId: MESSAGINGSENDERID,
-    appId: APPID
+    apiKey: "API_KEY",
+    authDomain: "AUTH_DOMAIN",
+    projectId: "PROJECT_ID",
+    storageBucket: "STORAGE_BUCKET",
+    messagingSenderId: "MESSAGING_SENDER_ID",
+    appId: "APP_ID",
 };
+
 firebase.initializeApp(firebaseConfig);
 
 const initApp = () => {
@@ -39,21 +40,136 @@ document.addEventListener("DOMContentLoaded", initApp);
 const handleDrop = (e) => {
     const dt = e.dataTransfer;
     const files = dt.files;
-    const firstFile = files[0];
 
-    let storageRef = firebase.storage().ref(firstFile.name);
-    let uploadTask = storageRef.put(firstFile);
+    Array.from(files).forEach((file) => {
+        let randomInt = Math.floor(Math.random() * 1000000);
+        let fileExtension = file.name.split('.').pop();
+        let fileName = file.name.split('.').slice(0, -1).join('.') + '_' + randomInt + '.' + fileExtension;
 
-    uploadTask.on("state_changed", (snapshot) => {
-        let progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-        console.log('Upload is ' + progress + '% done');
-    }, (error) => {
-        console.error(error);
-    }, () => {
-        uploadTask.snapshot.ref.getDownloadURL().then((downloadURL) => {
-            console.log('File available at', downloadURL);
-            document.getElementById('hiddenInput').value = downloadURL;
+        // Use the file extension to create the storage reference
+        let storageRef = firebase.storage().ref(fileExtension + '/' + fileName);
+        let uploadTask = storageRef.put(file);
+
+        uploadTask.on("state_changed", (snapshot) => {
+            let progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+
+            var loadingBar = document.getElementById('loadingBar');
+            loadingBar.style.width = progress + '%'; // Set the width to 50%
+            loadingBar.textContent = progress.toFixed(2) + '%';
+
+            if (progress == 100) {
+                loadingBar.textContent = 'File(s) uploaded successfully!';
+
+            }
+
+            console.log('Upload is ' + progress + '% done');
+        }, (error) => {
+            console.error(error);
+        }, () => {
+            uploadTask.snapshot.ref.getDownloadURL().then((downloadURL) => {
+                console.log('File available at', downloadURL);
+            });
         });
     });
+}
 
+document.addEventListener("DOMContentLoaded", function () {
+    initApp();
+
+    // Get a reference to the storage service
+    var storage = firebase.storage();
+
+    // Create a storage reference from our storage service
+    var storageRef = storage.ref();
+
+    // List all the prefixes (directories)
+    storageRef.listAll().then(function (res) {
+        var select = document.getElementById('fileTypeSelect');
+
+        res.prefixes.forEach(function (folderRef) {
+            // Get the folder name (file type)
+            var fileType = folderRef.name;
+
+            // Create a new option element
+            var option = document.createElement('option');
+            option.value = fileType;
+            option.text = fileType;
+
+            // Add the option to the select
+            select.appendChild(option);
+        });
+    }).catch(function (error) {
+        console.log('Error getting directories:', error);
+    });
+});
+
+// Add listFilesInDirectory to the global scope
+window.listFilesInDirectory = listFilesInDirectory;
+
+function listFilesInDirectory(directory) {
+    var storage = firebase.storage();
+    var storageRef = storage.ref();
+    var directoryRef = storageRef.child(directory);
+
+    directoryRef.listAll().then(function (res) {
+        var fileList = document.getElementById('fileList');
+
+        // Clear the file list
+        fileList.innerHTML = '';
+
+        res.items.forEach(function (fileRef) {
+            fileRef.getDownloadURL().then(function (url) {
+                // Create a new list item
+                var li = document.createElement('li');
+                li.className = 'p-1';
+
+                // Create a new link
+                var a = document.createElement('a');
+                a.href = url;
+                a.className = 'fs-5 text-decoration-none'
+
+                // Split the filename into parts based on the underscore and dot characters
+                var filenameParts = fileRef.name.split(/[_\.]/);
+                // Remove the ID from the filename
+                var filenameWithoutId = filenameParts.slice(0, -2).concat(filenameParts.slice(-1)).join('.');
+                a.textContent = filenameWithoutId;
+
+                // Add the link to the list item
+                li.appendChild(a);
+
+                // Create a new delete button
+                var button = document.createElement('button');
+                button.className = 'btn btn-danger ms-2 btn-sm rounded-circle';
+
+                // Create a new icon
+                var icon = document.createElement('i');
+                icon.className = 'fas fa-times'; // Font Awesome class for the 'X' icon
+
+                // Add the icon to the button
+                button.appendChild(icon);
+
+
+                button.onclick = function () {
+                    // Delete the file
+                    fileRef.delete().then(function () {
+                        // File deleted successfully
+                        console.log('File deleted successfully');
+                        // Refresh the file list
+                        listFilesInDirectory(directory);
+                    }).catch(function (error) {
+                        // An error occurred
+                        console.log('Error deleting file:', error);
+                    });
+                };
+
+                // Add the delete button to the list item
+                li.appendChild(button);
+
+                // Add the list item to the file list
+                fileList.appendChild(li);
+            });
+        });
+    }).catch(function (error) {
+        console.log('Error getting files:', error);
+    });
 }
--- Scripts/php/convert.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$api = new Ilovepdf('ILOVEPDF_PUBLIC_KEY','ILOVEPDF_PRIVATE_KEY');
-
-$myTaskConvertOffice = $ilovepdf->newTask('officepdf');
-
-$file=$myTask->addURLFile('https://URLpath/to/file_name.pdf');
-
-?>
